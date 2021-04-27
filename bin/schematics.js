05653b7c8491a6077131f8df997680c60a1ff220
Angular Builds <circleci@angular.io>
2021-04-27 12:07:03 +0000

    cb0561a9a ci: remove `commitMessage` from Renovate configuration

--- bin/schematics.js
+++ bin/schematics.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = void 0;
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.main = void 0;
 // symbol polyfill must go first
 require("symbol-observable");
 // tslint:disable-next-line:ordered-imports import-groups
