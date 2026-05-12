2fb7fa3e968d7f61402379b484eba6417a92d1f5
Angular Builds <dev-infra@angular.dev>
2026-05-12 18:31:27 +0000

    ea95e1a fix(@angular/ssr): remove stateful flag from URL_PARAMETER_REGEXP

--- bin/schematics.d.ts
+++ bin/schematics.d.ts
@@ -6,9 +6,10 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
+import { ProcessOutput } from '@angular-devkit/core/node';
 export interface MainOptions {
     args: string[];
-    stdout?: NodeJS.WritableStream;
-    stderr?: NodeJS.WritableStream;
+    stdout?: ProcessOutput;
+    stderr?: ProcessOutput;
 }
 export declare function main({ args, stdout, stderr, }: MainOptions): Promise<0 | 1>;
